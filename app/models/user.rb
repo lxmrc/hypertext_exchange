b2948b54d42696cee03d5a09236fb508314d9226
Alex Mercea <alex@lxmrc.com>
2023-04-28 21:50:31 +1000

    Install FriendlyId

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  extend FriendlyId
+  friendly_id :username, use: :slugged
+
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_many :links
 end
