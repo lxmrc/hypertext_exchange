fd24450a7151b693401b2dfb36d5217a0313e3a4
Alex Mercea <alex@lxmrc.com>
2023-04-28 22:45:54 +1000

    Update views and add 'about' page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :links
+  get "about", to: "static_pages#about"
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :links
+  root "links#index"
 end
