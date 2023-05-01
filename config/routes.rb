eaf7d15bded9dba62170e4e567f4bac8922ae17a
Alex Mercea <alex@lxmrc.com>
2023-05-01 15:59:50 +1000

    Add user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :comments
-  get "about", to: "static_pages#about"
   devise_for :users
+  get "users/:id", to: "users#show", as: "user"
 
   resources :links
+  resources :comments
+
   root "links#index"
+  get "about", to: "static_pages#about"
 end
